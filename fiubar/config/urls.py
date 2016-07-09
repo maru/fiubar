69294d5e671cb3cd0131f6dbcd058c1d3bda1e47
maru <maru@users.noreply.github.com>
2016-07-09 09:39:21 +0200

    static urls for dev

--- fiubar/config/urls.py
+++ fiubar/config/urls.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView, RedirectView
 from django.views import defaults as default_views
@@ -38,9 +37,13 @@ urlpatterns = [
     # Your stuff: custom urls includes go here
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
+
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.conf.urls.static import static
+
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
@@ -49,4 +52,4 @@ if settings.DEBUG:
         url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
         url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
         url(r'^500/$', default_views.server_error),
-    ]
+    ] + staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- fiubar/config/wsgi.py
+++ fiubar/config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fiubar.config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fiubar.config.settings.production")
 
 application = get_wsgi_application()
