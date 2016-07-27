1375aa339b7a24c6d2c644f6f1fd23abee2e375a
maru <github@s106.org>
2016-07-27 19:55:18 +0200

    Pages, css, images

--- fiubar/config/views.py
+++ fiubar/config/views.py
@@ -6,7 +6,7 @@ def home(request):
 	Home page: choose template based on logged-in user / new user.
 	"""
 	if request.user.is_authenticated():
-		template_file = 'pages/home.html'	
+		template_file = 'pages/home.html'
 	else:
 		template_file = 'pages/index.html'
 
