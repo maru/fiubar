410652e381679249e7db7bb46021701ecfea4a47
maru <github@s106.org>
2016-07-31 18:57:06 +0200

    Added SignupClosedAdapter model; testing

---
+++ fiubar/config/models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from allauth.account.adapter import DefaultAccountAdapter
+
+class SignupClosedAdapter(DefaultAccountAdapter):
+    def is_open_for_signup(self, request):
+        return False
