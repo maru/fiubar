e107f9dcdf7a0299d11f2c29161df481db9d2eed
maru <github@s106.org>
2016-08-03 10:16:33 +0200

    Only 1 password field for signup

--- fiubar/forms.py
+++ fiubar/forms.py
@@ -3,13 +3,14 @@ from django import forms
 from captcha.fields import ReCaptchaField
 
 
-class AllauthSignupForm(forms.Form):
+class SignupForm(forms.Form):
     """
     Signup form with recaptcha field.
     """
     captcha = ReCaptchaField()
 
-    field_order = ['username', 'email', 'password1', 'password2', 'captcha',]
+    field_order = ['username', 'email', 'password1',  'captcha',]
+
 
     def signup(self, request, user):
         """ Required, or else it throws deprecation warnings """
