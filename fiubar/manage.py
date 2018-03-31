c92d4d07362d5fa0c6f5304a1d1e667841ae1ce0
maru <github@s106.org>
2018-04-01 00:39:20 +0200

    Elimino hardcoded paths

--- fiubar/manage.py
+++ fiubar/manage.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 import os
-from os.path import abspath, dirname, join
 import sys
-
-BASE_DIR = dirname(dirname(abspath(__file__)))
-sys.path = [ BASE_DIR, BASE_DIR + '/Django-1.2.7'] + sys.path
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fiubar.settings")
